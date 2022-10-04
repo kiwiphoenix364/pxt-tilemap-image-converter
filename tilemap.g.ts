fbecbaca53c7c19f700ebe96b7a721abbc24d335
Kiwiphoenix <98428865+kiwiphoenix364@users.noreply.github.com>
2022-10-03 23:17:01 -0400

    Update tilemap.g.ts

--- tilemap.g.ts
+++ tilemap.g.ts
@@ -1,7 +1,6 @@
 // Auto-generated code. Do not edit.
 namespace myTiles {
     //% fixedInstance jres blockIdentity=images._tile
-    export const transparency16 = image.ofBuffer(hex``);
 
     helpers._registerFactory("tile", function(name: string) {
         switch(helpers.stringTrim(name)) {
