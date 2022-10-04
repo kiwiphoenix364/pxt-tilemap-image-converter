5d2893e1c55cbe371813151a656a5ea22b00cf4b
Kiwiphoenix <98428865+kiwiphoenix364@users.noreply.github.com>
2022-10-03 22:47:36 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,20 @@
+namespace redrawImg {
+    //% block
+    //% block="generate image From tilemap $selectedTilemap to image $outputImage"
+    export function GenerateImage(outputImage: Image, selectedTilemap: any) {
+        for (let x = 0; x < 255; x++) {
+            for (let y = 0; y < 255; y++) {
+                drawImg(x * 16, y * 16, selectedTilemap.tileImageAtLocation(selectedTilemap.getTileLocation(x, y)), outputImage)
+            }
+        }
+    }
+    function drawImg(x: number, y: number, startImg: Image, targetImg: Image) {
+        for (let xIdx = 0; xIdx < 16; xIdx++) {
+            for (let yIdx = 0; yIdx < 16; yIdx++) {
+                if (startImg.getPixel(xIdx, yIdx) != 0) {
+                    targetImg.setPixel(x + xIdx, y + yIdx, startImg.getPixel(xIdx, yIdx))
+                }
+            }
+        }
+    }
+}
