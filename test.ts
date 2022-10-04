5a734566048b225e557d534a5c01a6cfbee5136b
Kiwiphoenix <98428865+kiwiphoenix364@users.noreply.github.com>
2022-10-03 23:12:38 -0400

    Update tilemap.g.jres, tilemap.g.ts, test.ts

--- test.ts
+++ test.ts
@@ -1,1 +1,4 @@
 // tests go here; this will not be compiled when this package is used as an extension.
+let image2 = image.create(1000, 1000)
+tiles.setCurrentTilemap(tilemap`level2`)
+tilemapToImg.GenerateImage(image2)
