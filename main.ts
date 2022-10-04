7b2d8141b5c0c13ae8a6281a6b12240450bbd7f9
Kiwiphoenix <98428865+kiwiphoenix364@users.noreply.github.com>
2022-10-03 23:26:07 -0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -2,11 +2,14 @@ namespace tilemapToImg {
     //% block
     //% block="generate image from  tilemap $inputTilemap to image $outputImage"
     export function GenerateImage(inputTilemap: any, outputImage: any) {
+        let tilemap = game.currentScene().tileMap.data
+        tiles.setCurrentTilemap(inputTilemap)
         for (let x = 0; x < 255; x++) {
             for (let y = 0; y < 255; y++) {
-                drawImg(x * 16, y * 16, inputTilemap.tileImageAtLocation(inputTilemap.getTileLocation(x, y)), outputImage)
+                drawImg(x * 16, y * 16, tiles.tileImageAtLocation(tiles.getTileLocation(x, y)), outputImage)
             }
         }
+        tiles.setCurrentTilemap(tilemap)
     }
     function drawImg(x: number, y: number, startImg: Image, targetImg: Image) {
         for (let xIdx = 0; xIdx < 16; xIdx++) {
