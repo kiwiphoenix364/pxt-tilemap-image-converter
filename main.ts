c37d06a3380a0441751b8a002eb18acce3aaba34
kiwiphoenix <kiwiphoenix364@gmail.com>
2024-08-28 17:59:07 -0400

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -2,30 +2,9 @@ namespace tilemapToImg {
     //% block
     //% block="generate image from  tilemap $inputTilemap to image $outputImage"
     export function generateImage(inputTilemap: any, outputImage: any) {
-        let tilemap1 = inputTilemap
-        let tilemapLoaded = false
-        if (game.currentScene().tileMap) {
-            tilemapLoaded = true
-            tilemap1 = game.currentScene().tileMap.data
-        }
-        tiles.setCurrentTilemap(inputTilemap)
-        for (let x = 0; x < 255; x++) {
-            for (let y = 0; y < 255; y++) {
-                drawImg(x * 16, y * 16, tiles.tileImageAtLocation(tiles.getTileLocation(x, y)), outputImage)
-            }
-        }
-        if (tilemapLoaded) {
-            tiles.setCurrentTilemap(tilemap1)
-        } else {
-            game.currentScene().tileMap.setData(undefined);
-        }
-    }
-    function drawImg(x: number, y: number, startImg: Image, targetImg: Image) {
-        for (let xIdx = 0; xIdx < 16; xIdx++) {
-            for (let yIdx = 0; yIdx < 16; yIdx++) {
-                if (startImg.getPixel(xIdx, yIdx) != 0) {
-                    targetImg.setPixel(x + xIdx, y + yIdx, startImg.getPixel(xIdx, yIdx))
-                }
+        for (let x = 0; x < inputTilemap.width; x++) {
+            for (let y = 0; y < inputTilemap.height; y++) {
+                helpers.imageBlit(outputImage, x * 16, x * 16, x * 16 + 15, x * 16 + 15, inputTilemap.tileImageAtLocation(inputTilemap.getTileLocation(x, y)), 0, 0, 16, 16, true, false)
             }
         }
     }
